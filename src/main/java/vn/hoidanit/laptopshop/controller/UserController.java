d9ae4e15e110d55f893ac85ecd3ec81a2b6928c3
hothai19o5 <supperbroku@gmail.com>
2024-10-03 01:03:10 +0700

    Spring MVC

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,36 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "eric.html";
+    }
+}
+
+// @RestController
+// public class UserController {
+// // Dependency injection
+// // Khong nen dung @Autowired
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping(" ")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
